5cae65983bcccf254f6943b16301f4a769d9c94b
Maga Harutyunyan <magaharutyunyan969@gmail.com>
2025-04-09 21:44:44 +0400

    Provided initial collision detection

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -4,23 +4,66 @@ import { useRef, useEffect } from 'react';
 const GameBoard = () => {
     const canvasRef = useRef(null);
 
-    const fillCanvas = () => {
+    const width = 500;
+    const height = 500;
+
+    const BALL_SIZE = 7;
+    let ballPosition = {x: 20, y: 30};
+    
+    let xSpeed = 4;
+    let ySpeed = 2;
+
+    const fillCanvas = (ctx) => {
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, width, height);
+    };
+
+    const drawBall = (ctx) => {
+        ctx.fillStyle = "white";
+        ctx.fillRect(ballPosition.x, ballPosition.y, BALL_SIZE, BALL_SIZE);
+    };
+
+    const updateCanvas = () => {
+        ballPosition.x += xSpeed;
+        ballPosition.y += ySpeed;
+    };
+
+    const checkCollision = () => {
+        let ball = {
+            left: ballPosition.x,
+            right: ballPosition.x + BALL_SIZE,
+            top: ballPosition.y,
+            bottom: ballPosition.y + BALL_SIZE,
+        };
+
+        if (ball.left < 0 || ball.right > width) {
+            xSpeed = -xSpeed;
+        }
+
+        if (ball.top < 0 || ball.bottom > height) {
+            ySpeed = -ySpeed;
+        }
+    }
+
+    const gameLoop = () => {
+        console.log('drawing')
         const canvas = canvasRef.current;
         const ctx = canvas.getContext('2d');
 
-        let width = canvas.width;
-        let height = canvas.height;
+        fillCanvas(ctx);
+        drawBall(ctx);
+        updateCanvas();
+        checkCollision();
 
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, width, height);
-    }
+        setTimeout( gameLoop, 30);
+    };
 
     useEffect(() => {
-        fillCanvas();
+        gameLoop();
     }, []);
 
   return (
-    <canvas ref={canvasRef} id="canvas" width="500" height="500"></canvas>
+    <canvas ref={canvasRef} id="canvas" width={width} height={height}></canvas>
   );
 }
 
